721b7796b814560143a87c82c8f16a66de962407
Dmitry Danilov <dmitry.danilov@softformance.com>
2018-04-22 01:20:50 +0300

    Update Report model + Add new models

--- great_reports/reports/admin.py
+++ great_reports/reports/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Report
+from .models import Plan, Note, Report
+
+
+class NoteInline(admin.TabularInline):
+    model = Note
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    inlines = [NoteInline]
 
 
 @admin.register(Report)
--- great_reports/reports/models.py
+++ great_reports/reports/models.py
@@ -3,6 +3,46 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
+class Plan(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, verbose_name=_('User'))
+    date_for = models.DateField(verbose_name=_('Date for'))
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Created at'))
+
+    class Meta:
+        verbose_name = _('Plan')
+        verbose_name_plural = _('Plans')
+
+    def __str__(self):
+        return f'Plan by {self.owner} for {self.date_for}'
+
+
+class Note(models.Model):
+    plan = models.ForeignKey(
+        Plan, related_name='plans',
+        on_delete=models.CASCADE, verbose_name=_('Plan'))
+    title = models.CharField(verbose_name=_('Title'), max_length=250)
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, verbose_name=_('User'))
+    description = models.TextField(verbose_name=_('Description'))
+    estimate = models.TimeField(
+        verbose_name=_('Estimated time'), null=True, blank=True)
+    spent_time = models.TimeField(
+        verbose_name=_('Spent time'), null=True, blank=True)
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Created at'))
+    updated = models.DateTimeField(verbose_name=_('Updated'), auto_now=True)
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = _('Note')
+        verbose_name_plural = _('Notes')
+
+    def __str__(self):
+        return f'By {self.owner}: {self.title}'
+
+
 class Report(models.Model):
     text = models.CharField(verbose_name=_('Title'), max_length=250)
     owner = models.ForeignKey(
@@ -10,6 +50,8 @@ class Report(models.Model):
         verbose_name=_('User'))
     description = models.TextField(
         verbose_name=_('Description'), null=True, blank=True)
+    estimate = models.TimeField(
+        verbose_name=_('Estimated time'), null=True, blank=True)
     spent_time = models.TimeField(
         verbose_name=_('Spent time'), null=True, blank=True)
     created = models.DateTimeField(
